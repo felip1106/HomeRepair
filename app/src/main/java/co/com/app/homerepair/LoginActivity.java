4d58a3301b8d5a29dbad54fda6b15abec6b1ab88
Juan Mosquera <juanmosquera06@gmail.com>
2018-01-28 16:23:09 -0500

    Activity Login <Diseño>

--- app/src/main/java/co/com/app/homerepair/LoginActivity.java
+++ app/src/main/java/co/com/app/homerepair/LoginActivity.java
@@ -3,12 +3,12 @@ package co.com.app.homerepair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_login);
     }
 }
